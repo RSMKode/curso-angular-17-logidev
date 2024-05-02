d5c245476f3a7f0f9990c0a0f28ffe331234a717
Roger <rogersanchom@gmail.com>
2024-05-02 08:54:24 +0200

    Añadir estilos globales para el esquema de color claro y oscuro en styles.scss, app.config.ts, index.html, package.json, angular.json, app.component.ts, task-list.component.scss, task-list.component.html, app.component.html.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TaskListComponent } from './task-list/task-list.component';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TaskListComponent],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatTooltipModule,
+    MatButtonToggleModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
